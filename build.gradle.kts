46d8b84f45da98a61205eb1d43dcf0104d9ca9e0
pratikgaur <pratikgaur21@gmail.com>
2023-06-24 08:28:17 +0200

    running flyway migration on application startup in an effort to dockerize the service

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
-import java.io.File
+
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import java.io.FileInputStream
 import java.util.*
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("org.flywaydb.flyway") version "9.3.1"
@@ -38,6 +38,7 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("org.flywaydb:flyway-core:6.4.1")
     runtimeOnly("org.postgresql:postgresql")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
