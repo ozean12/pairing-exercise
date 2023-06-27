495a6e3ec55d6b76600ccb73cef5227ac2993359
bigsolom <eslam.foad@gmail.com>
2023-06-27 02:00:42 +0200

    use jsonb instead of json as it's more efficient and allows better query options

---
+++ src/main/kotlin/io/billie/organisations/viewmodel/Address.kt
@@ -0,0 +1,4 @@
+package io.billie.organisations.viewmodel
+
+class Address {
+}
--- src/main/resources/db/migration/V9__add_adresses_table.sql
+++ src/main/resources/db/migration/V9__add_adresses_table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE IF NOT EXISTS organisations_schema.addresses
 (
     id          UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-    data        json   NOT NULL
+    data        jsonb   NOT NULL
 );
